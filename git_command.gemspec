819da9a2add504730ab675b36f50fcd2c34f1990
Cody Poll <cpoll@instructure.com>
2015-06-09 20:04:25 -0600

    Initial commit

---
+++ git_command.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+	s.name 			= "git_command"
+	s.version 		= "0.0.1"
+	s.date 			= "2015-06-09"
+	s.summary 		= "A small framework for creating custom git commands in ruby"
+	s.description 	= "A small framework for creating custom git commands in ruby"
+	s.authors 		= ['Cody Poll']
+	s.email 		= 'CJPoll@gmail.com'
+	s.files 		= "lib/git_command.rb"
+	s.homepage 		= ''
+	s.license 		= "MIT"
+end
---
+++ lib/git_command.rb
@@ -0,0 +1,44 @@
+def command command, &block
+	@commands ||= {}
+
+	@commands[command] = block
+
+	temp_args = ARGV
+	first = temp_args.shift
+
+	args = []
+	options = {}
+
+	is_option = false
+	option = ""
+
+	param = false
+	temp_args.each do |arg|
+		if is_option
+			options[option.to_sym] = arg
+			is_option = false
+		elsif option?(arg)
+			is_option = true
+			option = arg.gsub /--/, ''
+		elsif flag?(arg)
+			options[arg] = true
+		else
+			args << arg
+		end
+	end
+
+	args = [ nil ] if args = []
+
+	if first == command.to_s
+		block.call *args, **options
+	end
+end
+
+private
+def option?( option )
+	option.match /\A--/
+end
+
+def flag?( flag )
+	flag.match /\A-/
+end
